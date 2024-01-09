42e7d33401c512dc0071b91638ca4ae033043eca
Raffael Luthiger <r.luthiger@huanga.com>
2024-01-09 16:59:34 +0100

    - Adding a template for the general HTML - Added highlight.js for code highlighting

--- create_book.php
+++ create_book.php
@@ -4,13 +4,16 @@ require "vendor/autoload.php";
 #$Parsedown = new Parsedown();
 $Parsedown = new ParsedownExtra(); # Parsedown Extra handles code blocks etc better
 
+$template = file_get_contents('./template/template.html');
+
 // output all files and directories except for '.' and '..'
 foreach (new DirectoryIterator('./manuscript/en/') as $fileInfo) {
     if($fileInfo->isDot()) continue;
     if (!$fileInfo->isDir()) {
 	    $content = file_get_contents('./manuscript/en/' . $fileInfo->getFilename());
 	    $newFilename = substr_replace($fileInfo->getFilename() , 'html', strrpos($fileInfo->getFilename() , '.') +1);
-	    file_put_contents('./book/en/' . $newFilename, $Parsedown->text($content));
-    
+        $fullPage = str_replace("###CONTENT###", $Parsedown->text($content), $template);
+	    file_put_contents('./book/en/' . $newFilename, $fullPage);
+
     }
 }
